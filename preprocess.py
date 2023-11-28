002f9a382a81f08b9d578ee39e25616642a9a415
amrit2611 <dhandhariaamrit@gmail.com>
2023-11-28 18:12:32 +0530

    further preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,23 +1,30 @@
 import emoji
 import re
 from bs4 import BeautifulSoup
+from html import unescape  # Import unescape function
 
-def remove_html_tags(text):
-    soup = BeautifulSoup(text, 'html.parser')
-    return soup.get_text()
+def remove_html_tags_and_urls(text):
+    # Use BeautifulSoup to handle HTML entities
+    soup = BeautifulSoup(unescape(text), 'html.parser')
+
+    # Remove HTML tags and URLs
+    clean_text = re.sub(r'https?://\S+|www\.\S+|<.*?>', '', soup.get_text())
+
+    return clean_text
 
 def replace_emojis(comment):
-    return emoji.demojize(comment)
+    return ''.join(c if c not in emoji.UNICODE_EMOJI else ' ' for c in comment)
 
 def convert_to_lowercase(input_file, output_file):
-    with open(input_file, "r", encoding="utf-8") as f:
-        unique_comments = f.readlines()
+    with open(input_file, "rb") as f:
+        # Read bytes, decode to string, and remove leading/trailing whitespaces
+        unique_comments = [line.strip() for line in f.readlines()]
 
-    # Remove HTML tags and replace emojis
-    processed_comments = [replace_emojis(remove_html_tags(comment.lower())) for comment in unique_comments]
+    # Remove HTML tags, URLs, and replace emojis
+    processed_comments = [replace_emojis(remove_html_tags_and_urls(comment.decode("utf-8")).lower()) if isinstance(comment, bytes) else replace_emojis(remove_html_tags_and_urls(comment.lower())) for comment in unique_comments]
 
     with open(output_file, "w", encoding="utf-8") as f:
-        f.writelines(processed_comments)
+        f.writelines('\n'.join(processed_comments))
 
 if __name__ == "__main__":
     input_file = "unique_comments.txt"
